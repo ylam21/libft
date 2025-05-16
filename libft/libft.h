535b1987496b2339e9c5c21753852ae263dde9b3
Ondrej <ohnivasasanka@gmail.com>
2025-05-16 16:31:45 +0200

    redirection of files

--- libft/libft.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBFT_H
-#define LIBFT_H
-
-#include <string.h>
-void bzero(void *s, size_t n);
-void *ft_memset(void *s, int c, size_t n);
-
-#endif
--- libft/srcs/ft_bzero.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-
-void ft_bzero(void *s, size_t n) {
-	char *ptr = (char *)s;
-	while (n--) {
-		*(ptr++) = '\0';
-	}
-}
-
